5284f113f2d90ffdf864f4d2cf6fbdf0fc484d5c
Manuel Rocha <manueltemidorocha@gmail.com>
2020-02-17 22:37:41 +0000

    lab solved

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 const serveFavicon = require('serve-favicon');
 
+//Inserted by me
+const User = require('./models/user');
+const mongoose = require('mongoose');
+const expressSession = require('express-session');
+const ConnectMongo = require('connect-mongo');
+
+const mongoStore = ConnectMongo(expressSession);
+const hbs = require('hbs');
+//till here
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -15,6 +24,7 @@ const app = express();
 // Setup view engine
 app.set('views', join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + join('views/partials'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,7 +41,49 @@ app.use(
     sourceMap: true
   })
 );
+//inserted by me
 
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 15 * 24 * 60 * 60 * 1000
+    },
+    store: new mongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60
+    })
+  })
+);
+
+const deserializeUser = (req, res, next) => {
+  const userId = req.session.userId;
+  if (userId) {
+    User.findById(userId)
+      .then(user => {
+        if (!user) {
+          delete req.session.userId;
+        } else {
+          req.user = user;
+        }
+        next();
+      })
+      .catch(error => {
+        next(error);
+      });
+  } else {
+    next();
+  }
+};
+const passUserToViews = (req, res, next) => {
+  res.locals.user = req.user;
+  next();
+};
+//till here
+app.use(deserializeUser);
+app.use(passUserToViews);
 app.use('/', indexRouter);
 
 // Catch missing routes and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,127 @@
 const { Router } = require('express');
 const router = Router();
 
+const bcryptjs = require('bcryptjs');
+
+const User = require('./../models/user');
+
+const routeGuard = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    next(new Error('USER_NOT_AUTHORIZED'));
+  }
+};
+// router.use(routeGuard);
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello World!' });
+});
+
+router.get('/sign-in', (req, res, next) => {
+  res.render('authentication/sign-in');
+});
+
+router.post('/sign-in', (req, res, next) => {
+  const { email, password } = req.body;
+
+  let user;
+  User.findOne({ email })
+    .then(document => {
+      if (!document) {
+        next(new Error('USER_NOT_FOUND'));
+      } else {
+        user = document;
+        return bcryptjs.compare(password, document.passwordHash);
+      }
+    })
+    .then(match => {
+      if (match) {
+        req.session.userId = user._id;
+        res.redirect('/');
+      } else {
+        next(new Error('USER_PASSWORD_WRONG'));
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.get('/sign-up', (req, res, next) => {
+  res.render('authentication/sign-up');
+});
+
+router.post('/sign-up', (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  bcryptjs
+    .hash(password, 10)
+    .then(hashPlusSalt => {
+      return User.create({
+        email,
+        name,
+        passwordHash: hashPlusSalt
+      });
+    })
+    .then(user => {
+      req.session.userId = user._id;
+      res.redirect('/');
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.post('/sign-out', (req, res, next) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+router.get('/private', routeGuard, (req, res, next) => {
+  res.render('private');
+});
+router.get('/user', routeGuard, (req, res, next) => {
+  res.render('authentication/user');
+});
+router.get('/editProfile', routeGuard, (req, res, next) => {
+  res.render('authentication/editProfile');
+});
+router.post('/editProfile', routeGuard, (req, res, next) => {
+  const { email, password, name } = req.body;
+  let user;
+  User.findOne({ email })
+    .then(document => {
+      if (!document) {
+        next(new Error('USER_NOT_FOUND'));
+      } else {
+        user = document;
+        return bcryptjs.compare(password, document.passwordHash);
+      }
+    })
+    .then(match => {
+      if (match) {
+        User.findByIdAndUpdate(
+          {
+            _id: user._id
+          },
+          {
+            email,
+            name,
+            passwordHash: hashPlusSalt
+          }
+        );
+      } else {
+        next(new Error('USER_PASSWORD_WRONG'));
+      }
+    })
+    .then(userUpdated => {
+      req.session.userId = userUpdated._id;
+      res.redirect('/');
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
